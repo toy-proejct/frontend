20056d8f3a3d9c41bad924458921c740a3c3af1e
hyebin <sksp4334@naver.com>
2022-05-17 17:22:04 +0900

    feat: chat 가상데이터 추가

---
+++ src/components/chat/data/chatDummy.ts
@@ -0,0 +1,66 @@
+const chatData = [
+  {
+    id: 0,
+    title: "판매글 제목을 보여줍니다.",
+    lastChat: "마지막 텍스트를 보여줍니다.",
+    content: [
+      { text: "1번입니다.", chatId: 0, time: "Tue May 17 2022 16:40:23 GMT+0900 (한국 표준시)" },
+      {
+        text: "테스트합니다~~~",
+        chatId: 1,
+        time: "Tue May 17 2022 12:40:23 GMT+0900 (한국 표준시)",
+      },
+      {
+        text: "테스트합니다@@@@@@@@@@@@@@@@@@@@@@@@",
+        chatId: 2,
+        time: "Tue May 17 2022 08:40:23 GMT+0900 (한국 표준시)",
+      },
+    ],
+    modifiedAt: "2분 전",
+    createdAt: "2022T-05-17 00:00:00",
+    notReadCount: 0,
+  },
+  {
+    id: 1,
+    title: "황홍공방입니다.",
+    lastChat: "테스트합니다.",
+    content: [
+      { text: "2번입니다", chatId: 0, time: "Tue May 17 2022 16:40:23 GMT+0900 (한국 표준시)" },
+      {
+        text: "테스트합니다~~~",
+        chatId: 1,
+        time: "Tue May 17 2022 16:40:23 GMT+0900 (한국 표준시)",
+      },
+      {
+        text: "테스트합니다@@@@@@@@@@@@@@@@@@@@@@@@",
+        chatId: 2,
+        time: "Tue May 17 2022 16:40:23 GMT+0900 (한국 표준시)",
+      },
+    ],
+    modifiedAt: "2일 전",
+    createdAt: "2022T-05-17 00:00:00",
+    notReadCount: 2,
+  },
+  {
+    id: 2,
+    title: "청축판매합니다.",
+    lastChat: "굿입니다.",
+    content: [
+      { text: "3번입니다", chatId: 0, time: "Tue May 17 2022 16:40:23 GMT+0900 (한국 표준시)" },
+      {
+        text: "테스트합니다~~~",
+        chatId: 1,
+        time: "Tue May 17 2022 16:40:23 GMT+0900 (한국 표준시)",
+      },
+      {
+        text: "테스트합니다@@@@@@@@@@@@@@@@@@@@@@@@",
+        chatId: 2,
+        time: "Tue May 17 2022 16:40:23 GMT+0900 (한국 표준시)",
+      },
+    ],
+    modifiedAt: "2주 전",
+    createdAt: "2022T-05-17 00:00:00",
+    notReadCount: 5,
+  },
+]
+export default chatData

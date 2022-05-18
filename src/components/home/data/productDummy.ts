22ea30815dd8c2463fbcb1b9735b19c6db88c0e4
hyebin <sksp4334@naver.com>
2022-05-18 16:50:37 +0900

    feat: product dummy데이터 갯수 추가,

--- src/components/home/data/productDummy.ts
+++ src/components/home/data/productDummy.ts
@@ -95,6 +95,102 @@ const ProductData = [
     image: "/statics/productExample.jpg",
     likedAt: 5,
   },
+  {
+    boardId: 8,
+    name: "레오파드 청축",
+    title: "레오파드 청축 판매",
+    cost: 150000,
+    detail:
+      "저번에 선물받은 키보드입니다. 구매한 지는 1년지났지만 실사용은 한달정도 되는 것 같습니다",
+    purchasedTime: "2022-1-1",
+    createdAt: "2022-04-20 11:39:00",
+    image: "/statics/productExample.jpg",
+    likedAt: 15,
+  },
+  {
+    boardId: 9,
+    name: "레오파드 청축",
+    title: "커스텀 키보드 해피해킹을 판매중입니다. 관심있으시면 연락주세요.",
+    cost: 150000,
+    detail:
+      "저번에 선물받은 키보드입니다. 구매한 지는 1년지났지만 실사용은 한달정도 되는 것 같습니다",
+    purchasedTime: "2022-1-1",
+    createdAt: "2022-04-20 12:54:00",
+    image: "/statics/productExample.jpg",
+    likedAt: 5,
+  },
+  {
+    boardId: 10,
+    name: "레오파드 청축",
+    title: "커스텀 키보드 판매",
+    cost: 150000,
+    detail:
+      "저번에 선물받은 키보드입니다. 구매한 지는 1년지났지만 실사용은 한달정도 되는 것 같습니다",
+    purchasedTime: "2022-1-1",
+    createdAt: "2022-04-20 17:33:20",
+    image: "/statics/productExample.jpg",
+    likedAt: 5,
+  },
+  {
+    boardId: 11,
+    name: "레오파드 청축",
+    title: "로지텍 판매중입니다.",
+    cost: 150000,
+    detail:
+      "저번에 선물받은 키보드입니다. 구매한 지는 1년지났지만 실사용은 한달정도 되는 것 같습니다",
+    purchasedTime: "2022-1-1",
+    createdAt: "2022-04-21 11:36:20",
+    image: "/statics/productExample.jpg",
+    likedAt: 5,
+  },
+  {
+    boardId: 12,
+    name: "레오파드 청축",
+    title: "레오파드 청축 판매",
+    cost: 150000,
+    detail:
+      "저번에 선물받은 키보드입니다. 구매한 지는 1년지났지만 실사용은 한달정도 되는 것 같습니다",
+    purchasedTime: "2022-1-1",
+    createdAt: "2022-04-19 17:33:20",
+    image: "/statics/productExample.jpg",
+    likedAt: 5,
+  },
+  {
+    boardId: 13,
+    name: "레오파드 청축",
+    title: "레오파드 청축 판매",
+    cost: 150000,
+    detail:
+      "저번에 선물받은 키보드입니다. 구매한 지는 1년지났지만 실사용은 한달정도 되는 것 같습니다",
+    purchasedTime: "2022-1-1",
+    createdAt: "2022-04-19 17:33:20",
+    image: "/statics/productExample.jpg",
+    likedAt: 5,
+  },
+  {
+    boardId: 14,
+    name: "레오파드 청축",
+    title: "레오파드 청축 판매",
+    cost: 150000,
+    detail:
+      "저번에 선물받은 키보드입니다. 구매한 지는 1년지났지만 실사용은 한달정도 되는 것 같습니다",
+    purchasedTime: "2022-1-1",
+    createdAt: "2022-04-19 17:33:20",
+    image: "/statics/productExample.jpg",
+    likedAt: 5,
+  },
+  {
+    boardId: 15,
+    name: "레오파드 청축",
+    title: "레오파드 청축 판매",
+    cost: 150000,
+    detail:
+      "저번에 선물받은 키보드입니다. 구매한 지는 1년지났지만 실사용은 한달정도 되는 것 같습니다",
+    purchasedTime: "2022-1-1",
+    createdAt: "2022-04-15 17:33:20",
+    image: "/statics/productExample.jpg",
+    likedAt: 5,
+  },
 ]
 
 export default ProductData

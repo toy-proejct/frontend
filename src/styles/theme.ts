64e19abd33837bacd670535b82ee50e2da073bb0
yaulhae <gilga4@naver.com>
2022-05-20 10:10:34 +0900

    design : theme css (size,view)값 변경

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,18 @@
 const colors = {}
 
 const size = {
-  xLarge: "1100px",
+  xLarge: "1000px",
+}
+
+const view = {
+  tablet: '1024px',
+  mobile : '768px',
 }
 
 const theme = {
   colors,
   size,
+  view
 }
 
 export default theme

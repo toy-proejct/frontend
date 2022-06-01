6ae053de8feb0ce537ab64bda00e3ebe2d14c9c2
hyebin <sksp4334@naver.com>
2022-06-01 13:55:18 +0900

    feat: dotenv 추가

---
+++ src/config/dotenv.ts
@@ -0,0 +1,8 @@
+const envs = {
+  kakaoApiKey: "process.env.NEXT_PUBLIC_KAKAO_REAT_API",
+  naverRedirecUri: "process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI",
+  naverClientId: "process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,",
+  naverRedirectUri: "process.env.NEXT_PUBLIC_REDIRECT_URI",
+}
+
+export default envs

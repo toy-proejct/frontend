d8419bb317d592b66f14457983cba1e87c6f99c6
hyebin <sksp4334@naver.com>
2022-06-02 21:10:11 +0900

    feat: dotenvts tinymce, naver 추가

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -1,8 +1,21 @@
-const envs = {
-  kakaoApiKey: "process.env.NEXT_PUBLIC_KAKAO_REAT_API",
-  naverRedirecUri: "process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI",
-  naverClientId: "process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,",
-  naverRedirectUri: "process.env.NEXT_PUBLIC_REDIRECT_URI",
+type EnvType = {
+  kakaoClientId?: string
+  kakaoRedirecUri?: string
+  naverMapClientId?: string
+  naverRedirecUri?: string
+  naverAuthClientId?: string
+  tinymceApiKey?: string
+}
+
+const envs: EnvType = {
+  kakaoClientId: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID,
+  kakaoRedirecUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
+
+  naverMapClientId: process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID,
+  naverRedirecUri: process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI,
+  naverAuthClientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
+
+  tinymceApiKey: process.env.NEXT_PUBLIC_TINYMCE_API_KEY,
 }
 
 export default envs

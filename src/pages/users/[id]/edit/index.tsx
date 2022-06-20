f963db60e1f4a791d3adf2183e7b3f30443eb8aa
yaulhae <gilga4@naver.com>
2022-06-20 14:05:29 +0900

    design : #36 내 정보 설정 페이지 마크업(진행중)

---
+++ src/pages/users/[id]/edit/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Edit = () => {
+  return <EditContainer>안녕</EditContainer>
+}
+
+const EditContainer = styled.div`
+  padding-top: 45px;
+`
+
+export default Edit
